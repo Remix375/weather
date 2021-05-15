7c6d64fe7328242fef6225639f80df2127c3be7e
Remi Guillou <beaugossremi@gmail.com>
2021-05-15 15:59:14 +0200

    started page design + add info

--- resssource/code/script.js
+++ resssource/code/script.js
@@ -1,11 +1,12 @@
-const data = {
+const dataExample = {
     "list": [
         {
             "main": {
                 temp: 20
             },
             "weather": {
-                
+                id: "500",
+                "icon": "10n"
             }
         }
 
@@ -18,3 +19,34 @@ const data = {
 
 
 }
+
+
+const search = () => {
+    const searchBar = document.getElementById("search-bar");
+    /*
+        autres variables pour conditions
+    */
+
+
+    // function that calls to api
+    const data = dataExample
+    let ancestor = document.getElementById('meteos')
+    let descendents = ancestor.getElementsByTagName('*');
+
+
+    for (let i = 0; i < descendents.length; i++) {
+        let e = descendents[i]
+
+        
+
+
+
+
+
+    }
+    
+
+}
+
+
+search()
