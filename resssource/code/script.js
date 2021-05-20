328cd26ac37577fabcbc9cf4129f1cdde8ee75a3
Remi Guillou <beaugossremi@gmail.com>
2021-05-20 21:53:33 +0200

    got location

--- resssource/code/script.js
+++ resssource/code/script.js
@@ -19,17 +19,7 @@ const reqToServ = (place) => {
     let dataFetched = fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=0608a5168843251f4918bd0d742c0e19&units=metric`)
     .then(response => response.json())
     .then(respJson => {
-        console.log(respJson.cod)
-        let retList = []
-        respJson["list"].forEach(item => {
-            retList.push({
-                temp: item["main"]["temp"],
-                icon: item["weather"][0]["icon"],
-                day: item["dt_txt"]
-            })
-        })
-        console.log(retList)
-        return retList
+        return respJson
     })
     return dataFetched;
 }
@@ -52,62 +42,52 @@ const search = () => {
 
 
     reqToServ("paris").then(rep => {
-        let ancestor = document.getElementById('meteos')
-        let descendents = ancestor.getElementsByClassName('day-meteo');
-        //let data = rep;
-        //console.log("data", data)
-        //meteo.innerHTML = "";
-        let heu = "hey"
-        console.log(heu[1])
+
+        let placeData = rep['city']
+        console.log(placeData)
 
         let data = [];
 
         let dateForItem = 0;
-        rep.forEach(item => {
-            if (item["day"].substring(8, 10) !== dateForItem & (parseInt(item["day"].substring(11, 13)) >= 12)) {
-                console.log(item["day"].substring(8, 10))
+        rep["list"].forEach(item => {
+            console.log(item['dt_txt'])
+            if (item["dt_txt"].substring(8, 10) !== dateForItem & (parseInt(item["dt_txt"].substring(11, 13)) >= 12)) {
+                console.log(item["dt_txt"].substring(8, 10))
                 data.push(item)
-                dateForItem = item["day"].substring(8, 10) 
+                dateForItem = item["dt_txt"].substring(8, 10) 
             }
         })
     
+        
     
-        for (let i = 0; i < 5/*/descendents.length*/; i++) {
+        for (let i = 0; i < 5; i++) {
             
-
-
-
-            let element = descendents[i]
             let day_data = data[i]
+            
     
+            console.log(day_data)
             if (!day_data) {
                 return
             }
-            console.log(descendents)
     
             d = document.createElement('div')
             d.classList.add("day-meteo")
             d.innerHTML = `
                 <div class="weather-box">
                     <div class="logo-container">
-                        <img src="http://openweathermap.org/img/wn/${day_data["icon"]}@2x.png"}>
+                        <img src="http://openweathermap.org/img/wn/${day_data["weather"][0]["icon"]}@2x.png"}>
                     </div>
                     <div class="information center">
-                        <h1 classe="place">Paris</h1>
+                        <h1 classe="place">${placeData["name"]}</h1>
                         <div class="weather-information">
-                        <h2 class="temp">Temperature: ${day_data["temp"]}</h2>
-                        <h2>Day: ${day_data["day"]}</h2>
+                        <h2 class="temp">Temperature: ${day_data["main"]["temp"]}</h2>
+                        <h2>Day: ${day_data["dt_txt"]}</h2>
                         <!--other info-->
                     </div>
                 </div>
             `
     
             document.getElementById("meteos").appendChild(d)
-            console.log(d)
-    
-    
-            console.log(element)
-    
             //descendents.appendChild(element)
         }
 
@@ -122,11 +102,11 @@ const search = () => {
 
     });
     
-
+/*/
     let ancestor = document.getElementById('meteos')
     let descendents = ancestor.getElementsByClassName('day-meteo');
 
-    console.log("data", data)
+    //console.log("data", data)
     //meteo.innerHTML = "";
 
 
@@ -162,8 +142,10 @@ const search = () => {
         console.log(element)
 
         //descendents.appendChild(element)
+
     }
     
+    /*/
 
 }
 
