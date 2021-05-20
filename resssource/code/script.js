0ebc257ed1162b5a4dc0f80af3aa5985230c5677
Remi Guillou <beaugossremi@gmail.com>
2021-05-20 22:11:11 +0200

    deleted extra code

--- resssource/code/script.js
+++ resssource/code/script.js
@@ -1,20 +1,3 @@
-const dataExample = {
-    "list": [
-        {
-            temp: 20,
-
-            "icon": "10n"
-            
-        }
-    ]
-
-
-
-
-}
-
-
-
 const reqToServ = (place) => {
     let dataFetched = fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=0608a5168843251f4918bd0d742c0e19&units=metric`)
     .then(response => response.json())
@@ -78,9 +61,9 @@ const search = () => {
                         <img src="http://openweathermap.org/img/wn/${day_data["weather"][0]["icon"]}@2x.png"}>
                     </div>
                     <div class="information center">
-                        <h1 classe="place">${placeData["name"]}</h1>
+                        <h1 classe="place">${placeData["name"]} ${placeData["country"]}</h1>
                         <div class="weather-information">
-                        <h2 class="temp">Temperature: ${day_data["main"]["temp"]}</h2>
+                        <h2 class="temp">Temperature: ${day_data["main"]["temp"]}°C</h2>
                         <h2>Day: ${day_data["dt_txt"]}</h2>
                         <!--other info-->
                     </div>
@@ -101,51 +84,6 @@ const search = () => {
 
 
     });
-    
-/*/
-    let ancestor = document.getElementById('meteos')
-    let descendents = ancestor.getElementsByClassName('day-meteo');
-
-    //console.log("data", data)
-    //meteo.innerHTML = "";
-
-
-    for (let i = 0; i < descendents.length; i++) {
-        let element = descendents[i]
-        let day_data = data[i]
-
-        if (!day_data) {
-            return
-        }
-        console.log(descendents)
-
-        d = document.createElement('div')
-        d.classList.add("day-meteo")
-        d.innerHTML = `
-            <div class="weather-box">
-                <div class="logo-container">
-                    <img src="http://openweathermap.org/img/wn/${day_data["icon"]}@2x.png"}>
-                </div>
-                <div class="information center">
-                    <h1 classe="place">Paris</h1>
-                    <div class="weather-information">
-                    <h2 class="temp">${day_data["temp"]}</h2>
-                    <!--other info-->
-                </div>
-            </div>
-        `
-
-        document.getElementById("meteos").appendChild(d)
-        console.log(d)
-
-
-        console.log(element)
-
-        //descendents.appendChild(element)
-
-    }
-    
-    /*/
 
 }
 
